c97a450f68e0b0bfd0c52e804231e357acf3e8fd
Dmitrii Damyan <dmitriignc@gmail.com>
2024-11-26 14:28:03 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,46 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+  let result = 0;
+
+  return function adder(a) {
+    if (a === undefined) {
+      result = sum;
+
+      sum = 0;
+
+      return result;
+    } else {
+      sum += a;
+
+      result = sum;
+
+      return (b) => {
+        if (b === undefined) {
+          sum = 0;
+
+          return result;
+        } else {
+          sum += b;
+
+          result = sum;
+
+          return (c) => {
+            if (c === undefined) {
+              sum = 0;
+
+              return result;
+            } else {
+              sum += c;
+
+              result = sum;
+            }
+          };
+        }
+      };
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
